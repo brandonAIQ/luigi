6452b3c233e3f0c461832c58ab51b59e4e438cda
Brandon <brandon@actioniq.com>
2020-01-02 14:24:23 -0500

    logs

--- luigi/scheduler.py
+++ luigi/scheduler.py
@@ -482,12 +482,14 @@ class MySqlTaskState(object):
         pass # always persisted
 
     def get_active_tasks(self):
+        logger.info("Called method get_active_tasks!")
         session = self.session()
         db_res = session.query(DBTask).all()
         session.close()
         return (pickle.loads(t.pickled) for t in db_res)
 
     def get_active_tasks_by_status(self, *statuses):
+        logger.info("Called method get_active_tasks_by_status!")
         start = time.time()
         session = self.session()
         db_res = session.query(DBTask).filter(DBTask.status.in_(statuses)).all()
@@ -500,6 +502,7 @@ class MySqlTaskState(object):
         return res
 
     def get_active_task_count_for_status(self, status):
+        logger.info("Called method get_active_task_count_for_status!")
         if status:
             session = self.session()
             db_res = session.query(DBTask).filter(DBTask.status == status).count()
@@ -522,12 +525,14 @@ class MySqlTaskState(object):
         return None, None
 
     def num_pending_tasks(self):
+        logger.info("Called method num_pending_tasks!")
         session = self.session()
         db_res = session.query(DBTask).filter(DBTask.status.in_([PENDING, RUNNING])).count()
         session.close()
         return db_res
 
     def get_task(self, task_id, default=None, setdefault=None):
+        logger.info("Called method get_task!")
         if self.has_task(task_id):
             session = self.session()
             db_task = session.query(DBTask).filter(DBTask.task_id == task_id).first()
@@ -544,6 +549,7 @@ class MySqlTaskState(object):
             return default
 
     def has_task(self, task_id):
+        logger.info("Called method has_task!")
         session = self.session()
         db_res = session.query(DBTask).filter(DBTask.task_id == task_id).count() > 0
         session.close()
@@ -560,6 +566,7 @@ class MySqlTaskState(object):
         raise NotImplementedError("No batch running right now")
 
     def set_status(self, task, new_status, config=None):
+        logger.info("Called method set_status!")
         if new_status == FAILED:
             assert config is not None
 
