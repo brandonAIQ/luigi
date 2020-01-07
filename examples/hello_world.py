3c0f621ac4c5480174081a6179925e7272c806a0
Brandon <brandon@actioniq.com>
2020-01-07 11:25:58 -0500

    reset test task

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -10,17 +10,10 @@ If that does not work, see :ref:`CommandLine`.
 import luigi
 
 
-class IngestJobTask(luigi.Task):
-
-    customer_id = luigi.Parameter()
-    trace_id = luigi.Parameter()
-
-    accepts_messages = True
-
+class HelloWorldTask(luigi.Task):
+    task_namespace = 'examples'
 
     def run(self):
-    	import time
-    	time.sleep(100)
         print("{task} says: Hello world!".format(task=self.__class__.__name__))
 
 
